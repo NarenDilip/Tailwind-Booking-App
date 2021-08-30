b2027e380e8befdc1b857ebd177797257cc98613
schnell <snarendran89@gmail.com>
2021-08-30 17:51:45 +0530

    Design Updation and commited

---
+++ app/src/main/java/com/kos/tailwindbookingapp/ui/LoginScreen.kt
@@ -0,0 +1,14 @@
+package com.kos.tailwindbookingapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kos.tailwindbookingapp.R
+
+class LoginScreen :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
---
+++ app/src/main/java/com/kos/tailwindbookingapp/ui/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.kos.tailwindbookingapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.kos.tailwindbookingapp.R
+
+/**
+ * Created by schnell on 30,August,2021
+ */
+class SplashScreen : AppCompatActivity() {
+
+    private var startView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acivity_splashscreen)
+
+        startView = this.findViewById(R.id.start_view)
+
+        startView!!.setOnClickListener {
+            val intent = Intent(this@SplashScreen, LoginScreen::class.java);
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
