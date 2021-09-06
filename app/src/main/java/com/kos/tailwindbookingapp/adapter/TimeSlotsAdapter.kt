4d530cb2f0f27f8a4eeaaeb2de7fc9ca94f1e7fc
schnell <snarendran89@gmail.com>
2021-09-06 13:01:24 +0530

    Time Slot with popup window showing and hiding issue

--- app/src/main/java/com/kos/tailwindbookingapp/adapter/TimeSlotsAdapter.kt
+++ app/src/main/java/com/kos/tailwindbookingapp/adapter/TimeSlotsAdapter.kt
@@ -2,11 +2,11 @@ package com.kos.tailwindbookingapp.adapter
 
 import android.content.Context
 import android.graphics.Point
+import android.graphics.drawable.BitmapDrawable
 import android.view.*
-import android.widget.ListPopupWindow
+import android.view.ViewGroup
 import android.widget.PopupWindow
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kos.tailwindbookingapp.R
 import com.kos.tailwindbookingapp.Util
 import com.kos.tailwindbookingapp.model.LaneSession
+import kotlinx.android.synthetic.main.dialog_lane.view.*
+
 
 class TimeSlotsAdapter internal constructor(
     private val context: Context,
@@ -24,6 +26,7 @@ class TimeSlotsAdapter internal constructor(
 ) :
     RecyclerView.Adapter<TimeSlotsAdapter.ViewHolder>() {
     private var rowIndex = -1
+    var mPopupwindow: PopupWindow? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.view_time_slot, parent, false)
@@ -31,12 +34,35 @@ class TimeSlotsAdapter internal constructor(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val v: View = LayoutInflater.from(context).inflate(
+            R.layout.select_mins_extender,
+            view.findViewById<ConstraintLayout>(R.id.popupmessage),
+            false
+        )
+
+        mPopupwindow = PopupWindow(v, 300, 150, true)
+        mPopupwindow = PopupWindow(
+            v,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        val remainingTimeView = v.findViewById<TextView>(R.id.remainingTimeView)
+        remainingTimeView.text = getRemainingTime(laneSession = laneSession)
+
+        val extendView = v.findViewById<TextView>(R.id.extendView)
+        extendView.setOnClickListener {
+            mPopupwindow!!.dismiss()
+            callback.laneTimeExtend()
+        }
+
         try {
             val lane = timeSlots[holder.adapterPosition]
             holder.timeSlotRootView.setOnClickListener {
                 callback.viewTimeSlot(lane, holder.adapterPosition)
                 updateTimeView(holder.adapterPosition)
-                showPopup(holder)
+                showPopup(holder, mPopupwindow!!, v)
             }
             holder.renderView(lane)
             if (rowIndex == holder.adapterPosition) {
@@ -44,11 +70,24 @@ class TimeSlotsAdapter internal constructor(
                 holder.timeSlotView.setTextColor(ContextCompat.getColor(context, R.color.black))
                 holder.minLabelView.setTextColor(ContextCompat.getColor(context, R.color.black))
             } else {
-                if (laneSession.startedOn != null && validateTimeSlot(laneSession, position,
-                        timeSlots)) {
+                if (laneSession.startedOn != null && validateTimeSlot(
+                        laneSession, position,
+                        timeSlots
+                    )
+                ) {
                     holder.timeSlotRootView.isEnabled = false
-                    holder.timeSlotView.setTextColor(ContextCompat.getColor(context, R.color.app_lite_grey))
-                    holder.minLabelView.setTextColor(ContextCompat.getColor(context, R.color.app_lite_grey))
+                    holder.timeSlotView.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.app_lite_grey
+                        )
+                    )
+                    holder.minLabelView.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.app_lite_grey
+                        )
+                    )
                 } else {
                     holder.timeSlotRootView.setBackgroundResource(R.drawable.rectangle_shape_black)
                     holder.timeSlotView.setTextColor(ContextCompat.getColor(context, R.color.white))
@@ -98,22 +137,7 @@ class TimeSlotsAdapter internal constructor(
         }
     }
 
-
-    private fun showPopup(holder: ViewHolder) {
-        var mPopupwindow: PopupWindow? = null
-        val v: View = LayoutInflater.from(context).inflate(
-            R.layout.select_mins_extender,
-            view.findViewById<ConstraintLayout>(R.id.popupmessage),
-            false
-        )
-        val remainingTimeView = v.findViewById<TextView>(R.id.remainingTimeView)
-        remainingTimeView.text = getRemainingTime(laneSession = laneSession)
-
-        val extendView = v.findViewById<TextView>(R.id.extendView)
-        extendView.setOnClickListener {
-            mPopupwindow!!.dismiss()
-            callback.laneTimeExtend()
-        }
+    private fun showPopup(holder: ViewHolder, mPopupwindow: PopupWindow, v: View) {
 
         val p = Point()
         val location = IntArray(2)
@@ -124,23 +148,17 @@ class TimeSlotsAdapter internal constructor(
         val OFFSET_X = -150
         val OFFSET_Y = 0
 
-        mPopupwindow = PopupWindow(v, 300, 150, true)
+        mPopupwindow.setBackgroundDrawable(BitmapDrawable())
+        mPopupwindow.setOutsideTouchable(true)
+
         mPopupwindow.showAtLocation(
             holder.minLabelView,
             Gravity.NO_GRAVITY,
             p.x + OFFSET_X,
             p.y + OFFSET_Y
         )
-        mPopupwindow.setInputMethodMode(ListPopupWindow.INPUT_METHOD_NOT_NEEDED)
-        mPopupwindow.setBackgroundDrawable(null);
-        mPopupwindow.setFocusable(true);
-        mPopupwindow.setClippingEnabled(true);
-        mPopupwindow.setOnDismissListener(PopupWindow.OnDismissListener { // TODO Auto-generated method stub
-            Toast.makeText(context, "Dismissed", Toast.LENGTH_SHORT).show()
-        })
 
-        if (mPopupwindow.isShowing()) {
-        }
+
     }
 
     override fun getItemCount(): Int {
